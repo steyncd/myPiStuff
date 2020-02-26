ed4a9046c96960593e69cb8d1b75e8fd936454f5
Christo Steyn <christo.steyn@magnabc.co.za>
2020-02-26 10:54:49 +0200

    debugging changes

--- models/MqttSwitch.py
+++ models/MqttSwitch.py
@@ -15,32 +15,36 @@ class MqttSwitch(MqttDevice.MqttDevice) :
         action = str(payload.decode("utf-8")).strip().lower()
         print("HandleCommand::received command with topic ", topic, "and payload ", payload)
         if action=="on" or action=="1":
-            gpio.setup(self.__pin,gpio.OUT)
-            gpio.output(self.__pin, gpio.LOW)
-            self.getClient().publish(self.getStatusTopic,"ON")
-            self.setStatus("On")
-            print("HandleCommand::Turning ",self.getName()," on, pin: " + self.__pin)
+            print("HandleCommand::on command received")
+            # gpio.setup(self.__pin,gpio.OUT)
+            # gpio.output(self.__pin, gpio.LOW)
+            # self.getClient().publish(self.getStatusTopic,"ON")
+            # self.setStatus("On")
+            # print("HandleCommand::Turning ",self.getName()," on, pin: " + self.__pin)
         elif action=="off" or action=="0":
-            gpio.output(self.__pin, gpio.HIGH)
-            self.getClient().publish(self.getStatusTopic,"ON")
-            self.setStatus("Off")
-            print("HandleCommand::turning " + self.getName() + " off, pin: " + self.__pin)
+            print("HandleCommand::off command received")
+            # gpio.output(self.__pin, gpio.HIGH)
+            # self.getClient().publish(self.getStatusTopic,"ON")
+            # self.setStatus("Off")
+            # print("HandleCommand::turning " + self.getName() + " off, pin: " + self.__pin)
         elif action=="toggle":
-            if gpio.input(self.__pin) == gpio.HIGH:
-                gpio.output(self.__pin, gpio.LOW)
-            else:
-                gpio.output(self.__pin, gpio.HIGH)
-
-            self.setStatus("On" if self.__status == "Off" else "Off")
-            self.getClient().publish(self.getStatusTopic,self.getStatus)
-            print("HandleCommand::toggling " + self.getName() + " status, pin: " + self.__pin)
+            print("HandleCommand::toggle command received")
+            # if gpio.input(self.__pin) == gpio.HIGH:
+            #     gpio.output(self.__pin, gpio.LOW)
+            # else:
+            #     gpio.output(self.__pin, gpio.HIGH)
+            #
+            # self.setStatus("On" if self.__status == "Off" else "Off")
+            # self.getClient().publish(self.getStatusTopic,self.getStatus)
+            # print("HandleCommand::toggling " + self.getName() + " status, pin: " + self.__pin)
         elif action=="status":
-            print("HandleCommand::checking " + self.getName() + " status")
-            if gpio.input(pin) == gpio.HIGH:
-                self.setStatus("Off")
-            else:
-                self.setStatus("On")
-            self.PostStatus()
+            print("HandleCommand::Status command received")
+            # print("HandleCommand::checking " + self.getName() + " status")
+            # if gpio.input(pin) == gpio.HIGH:
+            #     self.setStatus("Off")
+            # else:
+            #     self.setStatus("On")
+            # self.PostStatus()
         else:
             print("ToggleGeyser::Command not recognized")
             self.getClient().publish(self.getStatusTopic,"Unknown action")
