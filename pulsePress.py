f3095b8c19f4b820ca8bb3ab675f5d951ada3abc
Christo Steyn <steyncd@gmail.com>
2020-03-01 07:45:54 +0200

    formatting fixes

--- pulsePress.py
+++ pulsePress.py
@@ -185,10 +185,10 @@ client.loop_start()
 
 print('Free RAM: ' + str(get_ram()[1]) + ' (' + str(get_ram()[0]) + ')')
 print('Nr. of processes: ' + str(get_process_count()))
-print('Up time: ' + get_up_stats()[0])
+print('Up time: ' + str(get_up_stats()[0].decode("utf-8")))
 print('Nr. of connections: ' + str(get_connections()))
 print('Temperature in C: ' + str(get_temperature()))
-print('IP-address: ' + str(get_ipaddress()))
+print('IP-address: ' + str(get_ipaddress().decode("utf-8")))
 print('CPU speed: ' + str(get_cpu_speed()))
 
 while True:
